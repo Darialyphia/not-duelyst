9b22ac8100b72ab361d850cb0824e139ab78fcea
loicpennequin <lo.pennequin@gmail.com>
2023-12-01 14:20:54 +0100

    channeled my inner Matt Pocock

--- libs/sdk/src/event/event.ts
+++ libs/sdk/src/event/event.ts
@@ -1,4 +1,4 @@
-import { JSONValue, Values } from '@hc/shared';
+import { Constructor, JSONValue, Values } from '@hc/shared';
 import { GameContext } from '../game';
 import { Serializable } from '../utils/interfaces';
 import { DealDamageEvent } from './deal-damage.event';
@@ -7,15 +7,32 @@ import { MoveEvent } from './move.event';
 import { SummonFromLoadoutEvent } from './summon-from-loadout.event';
 import { UseSkillEvent } from './use-sklll.event';
 
-export const eventMap = {
+export type EventName =
+  | 'END_TURN'
+  | 'MOVE'
+  | 'SUMMON_FROM_LOADOUT'
+  | 'USE_SKILL'
+  | 'DEAL_DAMAGE';
+
+type GenericEventMap = Record<EventName, Constructor<GameEvent<EventName, JSONValue>>>;
+
+type ValidatedEventMap<T extends GenericEventMap> = {
+  [Name in keyof T]: T[Name] extends Constructor<GameEvent<EventName, JSONValue>>
+    ? Name extends InstanceType<T[Name]>['name']
+      ? T[Name]
+      : never
+    : never;
+};
+
+const validateEventMap = <T extends GenericEventMap>(data: ValidatedEventMap<T>) => data;
+
+export const eventMap = validateEventMap({
   END_TURN: EndTurnEvent,
   MOVE: MoveEvent,
   SUMMON_FROM_LOADOUT: SummonFromLoadoutEvent,
   USE_SKILL: UseSkillEvent,
   DEAL_DAMAGE: DealDamageEvent
-} as const;
-
-export type EventName = keyof typeof eventMap;
+});
 
 export type SerializedEvent = ReturnType<
   InstanceType<Values<typeof eventMap>>['serialize']
