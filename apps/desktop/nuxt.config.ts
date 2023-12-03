6fa0722246c742cbcb013f7a406bff2bddd72693
loicpennequin <lo.pennequin@gmail.com>
2023-12-03 11:39:55 +0100

    setup nuxt eletron package

---
+++ apps/desktop/nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  appDir: "src",
+  devtools: { enabled: true },
+  extends: ["@hc/game-client"],
+  modules: ["nuxt-electron"],
+  electron: {
+    build: [
+      {
+        // Main-Process entry file of the Electron App.
+        entry: "electron/main.ts",
+      },
+    ],
+  },
+});
