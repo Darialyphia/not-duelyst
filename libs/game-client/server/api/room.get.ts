d303dd609ea0c444f7e773842c8722f48a4e027e
loicpennequin <lo.pennequin@gmail.com>
2024-01-14 20:20:52 +0100

    room url debugging

--- libs/game-client/server/api/room.get.ts
+++ libs/game-client/server/api/room.get.ts
@@ -15,24 +15,33 @@ export default defineEventHandler(async (event): Promise<string> => {
     return 'ws://localhost:8000';
   } else {
     const { roomId } = getQuery<{ roomId: string }>(event);
-    let tries = 0;
-    let exposedPort: ConnectionInfoV2['exposedPort'] = undefined;
-    do {
-      const response = await getHathoraClient().roomV2.getConnectionInfo(roomId);
-      if (response.connectionInfoV2?.status === 'active') {
-        exposedPort = response.connectionInfoV2!.exposedPort!;
-      } else {
-        console.log(`Room not activer, try ${tries} of ${GET_ROOM_POLLING_MAX_TRIES}`);
-        tries++;
-        if (tries > GET_ROOM_POLLING_MAX_TRIES) break;
-        await wait(GET_ROOM_POLLING_INTERVAL);
+    try {
+      let tries = 0;
+      let exposedPort: ConnectionInfoV2['exposedPort'] = undefined;
+      do {
+        const response = await getHathoraClient().roomV2.getConnectionInfo(roomId);
+        if (response.connectionInfoV2?.status === 'active') {
+          exposedPort = response.connectionInfoV2!.exposedPort!;
+        } else {
+          console.log(`Room not activer, try ${tries} of ${GET_ROOM_POLLING_MAX_TRIES}`);
+          tries++;
+          if (tries > GET_ROOM_POLLING_MAX_TRIES) {
+            throw new Error(`could not get the room url after ${tries} tries.`);
+          }
+          await wait(GET_ROOM_POLLING_INTERVAL);
+        }
+      } while (!exposedPort);
+
+      if (!exposedPort) {
+        throw new Error(`could not get the connection info for room ${roomId}`);
       }
-    } while (!exposedPort);
-
-    if (!exposedPort) {
-      throw new Error(`could not get the connection info for room ${roomId}`);
+      return `wss://${exposedPort?.host}:${exposedPort?.port}`;
+    } catch (err) {
+      throw createError({
+        message: `Could not get the connextion info for room ${roomId}`,
+        cause: err,
+        statusCode: 500
+      });
     }
-
-    return `wss://${exposedPort?.host}:${exposedPort?.port}`;
   }
 });
